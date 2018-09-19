109d4ac213c62c252a2c2fe46f402f704e89f14e
Stephen Mayeux <stephenmayeux@gmail.com>
2018-09-19 17:52:00 -0500

    click event from side

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
 import Nav from './Components/Nav'
 
-import axios from 'axios'
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      venues: [],
+      allMarkers: []
+    }
 
-  state = {
-    venues: [],
+    this.initMap = this.initMap.bind(this);
+    this.getVenues = this.getVenues.bind(this);
   }
 
   componentDidMount() {
-    this.getVenues('');
+    this.getVenues();
   }
 
   // load map script
-  loadMap = () => {
+  loadMap() {
     loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBDJfK_ZPEMHqgURHbksiFGt46JIW6wv0Y&callback=initMap")
     window.initMap = this.initMap;
     // handels console error for multipul api request
     window.google = {}
   }
-  
+
   // create a google map
-  initMap = () => {
+  initMap() {
 
     // created popup Window
     let infowindow = new window.google.maps.InfoWindow();
@@ -33,8 +39,10 @@ class App extends Component {
       zoom: 13
     });
 
+    const allMarkers = [];
+
     // display venues on map (dynamic markers)
-      this.state.venues.map(myVenue => {
+      this.state.venues.forEach(myVenue => {
         // display venue name in info window
         let contentString = `${myVenue.venue.name}`;
         // created a marker
@@ -42,25 +50,29 @@ class App extends Component {
           position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng},
           map: map,
           animation: window.google.maps.Animation.DROP,
-          title: myVenue.venue.name
+          title: myVenue.venue.name,
+          id: myVenue.venue.id
         });
-      
+
         // click on marker to display info window
-        marker.addListener('click', function() {
+        marker.addListener('click', function(event) {
+          console.log('click event?', event);
           // change the content
           infowindow.setContent(contentString);
           // open info window
           infowindow.open(map, marker);
         });
-          // Extend boundry on map for each marker
-          bounds.extend(marker.position);
+        // Extend boundry on map for each marker
+        bounds.extend(marker.position);
+        allMarkers.push(marker);
       })
-        // fit map to boundry
-        map.fitBounds(bounds);
-  } 
+      // fit map to boundry
+      map.fitBounds(bounds);
+      this.setState({ allMarkers });
+  }
 
   // Api call foursquare data
-  getVenues = (query) => {
+  getVenues(query = '') {
     const endPoint = 'https://api.foursquare.com/v2/venues/explore?';
     const params = {
       client_id: 'B0PVDBLLSY3FCNRHJRP25ZOHQK1B3TZMYH24PD45GYDZMYS1',
@@ -68,30 +80,41 @@ class App extends Component {
       query: query,
       near: 'Austin Texas',
       v: '20180505'
-    }
-  
+    };
+
     axios.get(endPoint + new URLSearchParams(params))
       .then(response => {
-        this.setState({
-          venues:response.data.response.groups[0].items
-        },this.loadMap());
-        }).catch(error => {
+        const { groups } = response.data.response;
+        this.setState({ venues: groups[0].items}, this.loadMap);
+        })
+        .catch(error => {
           console.log(error);
-        }) 
+        })
+    }
+
+  handleVenueClick(idx) {
+    const { allMarkers } = this.state;
+    window.google.maps.event.trigger(allMarkers[idx], 'click');
   }
 
   render() {
-
-    const listData = this.state.venues.map(venue =>  { 
-      return <li key={venue.venue.id}>
-                {venue.venue.name}
-             </li>})
+    const listData = this.state.venues.map((item, idx) => {
+      const { id, name } = item.venue;
+      return (
+        <li
+          key={id}
+          onClick={() => this.handleVenueClick(idx)}
+        >
+          {name}
+        </li>
+      );
+    });
 
     return (
         <main>
         <div className="">
           <Nav getVenues={this.getVenues} formHandler={this.formHandler}/>
-            <ul className="listView">{listData}</ul>
+          <ul className="listView">{listData}</ul>
           </div>
           <div id="map"></div>
         </main>
@@ -107,5 +130,3 @@ function loadScript(source) {
   script.defer = true;
   index.parentNode.insertBefore(script, index);
 }
-
-export default App;
